0b68c67f974fff30c96d504574d72472a670cb30
Kerem Dinçer <b.keremdincer@gmail.com>
2020-11-22 20:34:14 +0300

    Fixed publicPath #2

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,8 +6,7 @@ const packageJson = require('../package.json')
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js',
-    publicPath: '/container/latest/'
+    filename: '[name].[contenthash].js'
   },
   plugins: [
     new ModuleFederationPlugin({
